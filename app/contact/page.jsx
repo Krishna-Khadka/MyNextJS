2e693f1ccb9392e9437d85584f54eacd40462df2
Krishna-Khadka <krishkhadka2059@gmail.com>
2023-12-29 23:07:10 +0545

    Pages and Route Concept

---
+++ app/contact/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <h1 className='text-red-700 text-3xl font-bold'>Hello Conatct Me !!</h1>
+    )
+}
+
+export default Contact
